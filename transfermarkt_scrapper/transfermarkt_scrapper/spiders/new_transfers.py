3978009fda34b4d6844145cd63f878a92165c957
Dendi Handian <handian.dendi@gmail.com>
2021-08-09 01:58:42 +0700

    developing new_transfers scrapping

--- transfermarkt_scrapper/transfermarkt_scrapper/spiders/new_transfers.py
+++ transfermarkt_scrapper/transfermarkt_scrapper/spiders/new_transfers.py
@@ -4,7 +4,16 @@ import scrapy
 class NewTransfersSpider(scrapy.Spider):
     name = 'new_transfers'
     allowed_domains = ['transfermarkt.com']
-    start_urls = ['http://transfermarkt.com/']
+    start_urls = ['https://www.transfermarkt.com/transfers/neuestetransfers/statistik?land_id=0&wettbewerb_id=alle&minMarktwert=500000&maxMarktwert=200000000&plus=1']
 
     def parse(self, response):
-        pass
+        for i in range(1,10):
+            yield {
+                'name': 'Placeholder'
+            }
+        # for transfer in response.css('table.items tbody tr'):
+        #     yield {
+        #         'name': 'Placeholder'
+        #     }
+
+# poetry run scrapy crawl new_transfers -o results/new_transfers.json
