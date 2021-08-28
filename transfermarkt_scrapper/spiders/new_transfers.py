85b400fc2cee93060c9c566ccf138d322d8747dc
Dendi Handian <handian.dendi@gmail.com>
2021-08-29 01:57:28 +0700

    scraping page with js enabled - first move

--- transfermarkt_scrapper/spiders/new_transfers.py
+++ transfermarkt_scrapper/spiders/new_transfers.py
@@ -1,20 +1,27 @@
-import scrapy
+from scrapy import Spider
+from scrapy_splash import SplashRequest
 
-
-class NewTransfersSpider(scrapy.Spider):
+class NewTransfersSpider(Spider):
     name = 'new_transfers'
     allowed_domains = ['transfermarkt.com']
-    start_urls = ['https://www.transfermarkt.com/transfers/neuestetransfers/statistik?land_id=0&wettbewerb_id=alle&minMarktwert=500000&maxMarktwert=200000000&plus=1']
+    script = '''
+        function main(splash, args)
+            splash.private_mode_enabled = false
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+            splash:set_viewport_full()
+            return splash:html()
+        end
+    '''
+
+    def start_requests(self):
+        yield SplashRequest(url="https://www.transfermarkt.com/transfers/neuestetransfers/statistik?land_id=0&wettbewerb_id=alle&minMarktwert=500000&maxMarktwert=200000000&plus=1", callback=self.parse, endpoint="execute", args={'lua_source': self.script})
 
     def parse(self, response):
-        for i in range(1,10):
-            yield {
-                'name': 'Placeholder'
-            }
-        # for transfer in response.css('table.items tbody tr'):
-        #     yield {
-        #         'name': 'Placeholder'
-        #     }
+        print(response.body)
 
-# poetry run scrapy crawl new_transfers -o datasets/new_transfers.json
-# NOTE: scraping transfermarkt site require javascript-enabled, so we will use splash
+"""
+poetry run scrapy crawl new_transfers
+poetry run scrapy crawl new_transfers -o datasets/new_transfers.json
+"""
