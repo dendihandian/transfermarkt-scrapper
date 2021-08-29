d852305a9205383cc74b87a6f96156b384cdca06
Dendi Handian <handian.dendi@gmail.com>
2021-08-29 23:43:16 +0700

    scrapping the new transfer without pagination

--- transfermarkt_scrapper/spiders/new_transfers.py
+++ transfermarkt_scrapper/spiders/new_transfers.py
@@ -19,7 +19,25 @@ class NewTransfersSpider(Spider):
         yield SplashRequest(url="https://www.transfermarkt.com/transfers/neuestetransfers/statistik?land_id=0&wettbewerb_id=alle&minMarktwert=500000&maxMarktwert=200000000&plus=1", callback=self.parse, endpoint="execute", args={'lua_source': self.script})
 
     def parse(self, response):
-        print(response.body)
+        # print(response.body)
+
+        transfers = response.xpath("//table[contains(@class, 'items')][1]/tbody/tr")
+        for transfer in transfers:
+            yield {
+                'player_id': transfer.xpath("./td[1]/table/tbody/tr/td[2]/a/@id").get(),
+                'name': transfer.xpath("./td[1]/table/tbody/tr/td[2]/a/text()").get(),
+                'age': transfer.xpath("./td[2]/text()").get(),
+                'position': transfer.xpath("./td[1]/table/tbody/tr[2]/td[1]/text()").get(),
+
+                'left_club': transfer.xpath("./td[4]/table/tbody/tr[1]/td[2]/a/text()").get(),
+                'left_club_league': transfer.xpath("./td[4]/table/tbody/tr[2]/td/a/text()").get(),
+                'joined_club': transfer.xpath("./td[5]/table/tbody/tr[1]/td[2]/a/text()").get(),
+                'joined_club_league': transfer.xpath("./td[5]/table/tbody/tr[2]/td/a/text()").get(),
+
+                'transfer_date': transfer.xpath("./td[6]/text()").get(),
+                'market_value': transfer.xpath("./td[7]/text()").get(),
+                'fee': transfer.xpath("./td[8]/a/text()").get()
+            }
 
 """
 poetry run scrapy crawl new_transfers
