4803e2e47091628acaf3b1144605d6fa557ffd6c
Dendi Handian <handian.dendi@gmail.com>
2021-09-01 20:30:20 +0700

    new transfer pagination (doesnt follow)

--- transfermarkt_scrapper/spiders/new_transfers.py
+++ transfermarkt_scrapper/spiders/new_transfers.py
@@ -1,5 +1,6 @@
 from scrapy import Spider
 from scrapy_splash import SplashRequest
+import logging
 
 class NewTransfersSpider(Spider):
     name = 'new_transfers'
@@ -15,18 +16,28 @@ class NewTransfersSpider(Spider):
         end
     '''
 
-    def start_requests(self):
-        yield SplashRequest(url="https://www.transfermarkt.com/transfers/neuestetransfers/statistik?land_id=0&wettbewerb_id=alle&minMarktwert=500000&maxMarktwert=200000000&plus=1", callback=self.parse, endpoint="execute", args={'lua_source': self.script})
+    def start_requests(self, url=None):
+        if url:
+            yield SplashRequest(url=url, callback=self.parse, endpoint="execute", args={'lua_source': self.script})
+        else:
+            yield SplashRequest(url="https://www.transfermarkt.com/transfers/neuestetransfers/statistik?land_id=0&wettbewerb_id=alle&minMarktwert=500000&maxMarktwert=200000000&plus=1", callback=self.parse, endpoint="execute", args={'lua_source': self.script})
 
     def parse(self, response):
         # print(response.body)
 
         transfers = response.xpath("//table[contains(@class, 'items')][1]/tbody/tr")
         for transfer in transfers:
+
+            nationalities = []
+            for nationality in transfer.xpath("./td[3]/img"):
+                nationalities.append(nationality.xpath('./@title').get())
+            nationalities = '|'.join(nationalities)
+
             yield {
                 'player_id': transfer.xpath("./td[1]/table/tbody/tr/td[2]/a/@id").get(),
                 'name': transfer.xpath("./td[1]/table/tbody/tr/td[2]/a/text()").get(),
                 'age': transfer.xpath("./td[2]/text()").get(),
+                'nationalities': nationalities,
                 'position': transfer.xpath("./td[1]/table/tbody/tr[2]/td[1]/text()").get(),
 
                 'left_club': transfer.xpath("./td[4]/table/tbody/tr[1]/td[2]/a/text()").get(),
@@ -38,6 +49,15 @@ class NewTransfersSpider(Spider):
                 'market_value': transfer.xpath("./td[7]/text()").get(),
                 'fee': transfer.xpath("./td[8]/a/text()").get()
             }
+
+        next_page = response.xpath("//li[contains(@class, 'naechste-seite')]/a/@href").get()
+        logging.debug('next_page:')
+        logging.info(next_page)
+
+        if next_page:
+            logging.debug('masuk next page')
+            self.start_requests(url=next_page)
+
 
 """
 poetry run scrapy crawl new_transfers
