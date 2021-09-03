e905d1dc2ccafaf5f0f336afdaab780a3023f803
Dendi Handian <handian.dendi@gmail.com>
2021-09-03 12:09:47 +0700

    poetry run alembic revision -m "create_new_transfer_table"

---
+++ migrations/versions/19383297ecea_create_new_transfer_table.py
@@ -0,0 +1,24 @@
+"""create_new_transfer_table
+
+Revision ID: 19383297ecea
+Revises: 
+Create Date: 2021-09-03 12:09:21.847340
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '19383297ecea'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
