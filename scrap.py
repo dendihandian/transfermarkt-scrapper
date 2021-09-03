6b5b4615018f227ebf693ab00a5b9bb49b313830
Dendi Handian <handian.dendi@gmail.com>
2021-09-03 11:43:27 +0700

    add print logging

--- scrap.py
+++ scrap.py
@@ -2,4 +2,9 @@ import os
 from datetime import datetime
 
 filename = datetime.today().strftime("%Y_%m_%d_%H_%M_%S") + '.jl'
-os.system(f"poetry run scrapy crawl new_transfers -o datasets/new_transfer/{filename}")
+
+print("===== Started =====")
+
+os.system(f"poetry run scrapy crawl new_transfers -o datasets/new_transfers/{filename}")
+
+print("===== Finished =====")
