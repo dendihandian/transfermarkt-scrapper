9f02801f1b8c777e2824352ba1774dcce8f04ab1
Dendi Handian <handian.dendi@gmail.com>
2021-09-16 03:02:40 +0700

    poetry run alembic revision -m "create transfers table"

---
+++ migrations/versions/dddc92cf0023_create_transfers_table.py
@@ -0,0 +1,24 @@
+"""create transfers table
+
+Revision ID: dddc92cf0023
+Revises: 19383297ecea
+Create Date: 2021-09-16 03:02:26.805502
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dddc92cf0023'
+down_revision = '19383297ecea'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
